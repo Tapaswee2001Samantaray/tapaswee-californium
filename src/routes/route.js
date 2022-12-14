7dc949f9cb4286ec532c76ab995318b976705dfa
Tapaswee2001Samantaray <tapas10231@gmail.com>
2022-12-14 19:03:34 +0530

    Assignment on query params pushed on 14-12-22 at 07.04pm

--- src/routes/route.js
+++ src/routes/route.js
@@ -159,5 +159,49 @@ router.post( "/post-query-2", function (req, res){
     res.send( {data: finalArr , status: true})
 })
 
+let persons= [
+    {
+    name: "PK",
+    age: 10,
+    votingStatus: false
+ },
+ {
+    name: "SK",
+    age: 20,
+    votingStatus: false
+ },
+ {
+    name: "AA",
+    age: 70,
+    votingStatus: false
+ },
+ {
+    name: "SC",
+    age: 5,
+    votingStatus: false
+ },
+ {
+    name: "HO",
+    age: 40,
+    votingStatus: false
+ }
+ ]
+
+ //assignment on query params --> you will be given an array of persons ( i.e an array of objects )..each person will have  a {name: String , age: Number, votingStatus: true/false(Boolean)}
+// take input in query param as votingAge..and for all the people above that age, change votingStatus as true
+// also return an array consisting of only the person that can vote
+
+ router.post("/age" , function(req , res){
+    let personObj = req.query;
+    let newArr = [];
+    for(let i=0 ; i<persons.length ; i++){
+        if(persons[i].age >= personObj.votingAge){
+            persons[i].votingStatus = true;
+            newArr.push(persons[i]);
+        }
+    }
+    
+    res.send({data:newArr , status:true});
+ });
 
 module.exports = router;
